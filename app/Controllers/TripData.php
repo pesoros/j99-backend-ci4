e9c1017cb3af6f8c51352edecb71c19d6fcaa7f9
bayuyuhartono <bayuyuhartono@gmail.com>
2022-03-07 10:58:22 +0700

    fix

--- app/Controllers/TripData.php
+++ app/Controllers/TripData.php
@@ -14,6 +14,7 @@ class TripData extends ResourceController
     public function __construct()
     {
         $this->tripModel = new TripModel();
+        $this->db = \Config\Database::connect();
     }
 
     public function tripList()
@@ -26,18 +27,26 @@ class TripData extends ResourceController
         $kotaBerangkat = isset($bodyRaw['berangkat']) ? $bodyRaw['berangkat'] : '';
         $kotaTujuan = isset($bodyRaw['tujuan']) ? $bodyRaw['tujuan'] : '';
 
+        $tanggalBerangkat = date("Y-m-d", strtotime(!empty($tanggalBerangkat)?$tanggalBerangkat:date('Y-m-d')));
+
         $filterData = [
 			'start_point' => $kotaBerangkat,
 			'end_point'   => $kotaTujuan,
-			'date'        => date("Y-m-d", strtotime(!empty($tanggalBerangkat)?$tanggalBerangkat:date('Y-m-d'))),
+			'date'        => $tanggalBerangkat,
 			'fleet_type'  => $kelas
         ];
         $result = $this->tripModel->getTripList($filterData)->getResult();
 
-        return $this->respond($result, 200);;
+        foreach ($result as $key => $value) {
+            $checkSeat = $this->tripModel->checkSeatAvail($value->trip_id_no, $tanggalBerangkat)->getResult();
+            $value->seatPicked = $checkSeat[0]->picked; 
+            $value->seatAvail = intval($value->fleet_seats) - intval($checkSeat[0]->picked); 
+        }
+        
+        return $this->respond($result, 200);
     }
 
-    public function seatList()
+    public function seatList_old()
     {
         $separate = [2,6,10,14,18];
         for ($i=0; $i < 20; $i++) { 
@@ -60,6 +69,100 @@ class TripData extends ResourceController
             }
         }
         
+        return $this->respond($result, 200);
+    }
+
+    public function seatList()
+    {
+        $bodyRaw = $this->request->getRawInput();
+        $trip_route_id = isset($bodyRaw['trip_route_id']) ? $bodyRaw['trip_route_id'] : '';
+        $trip_id_no = isset($bodyRaw['trip_id_no']) ? $bodyRaw['trip_id_no'] : '';
+        $fleet_registration_id = isset($bodyRaw['fleet_registration_id']) ? $bodyRaw['fleet_registration_id'] : '';
+        $fleet_type_id = isset($bodyRaw['fleet_type_id']) ? $bodyRaw['fleet_type_id'] : '';
+        $booking_date = isset($bodyRaw['booking_date']) ? $bodyRaw['booking_date'] : '';
+
+        #--------------------------------------------------------
+        $currency = $this->tripModel->retrieve_currency()->getResult();
+        $data['trip_id_no'] = $trip_id_no;
+        $data['trip_route_id'] = $trip_route_id;
+        $data['fleet_registration_id'] = $fleet_registration_id;
+        $data['fleet_type_id'] = $fleet_type_id;
+        $data['booking_date'] = $booking_date;
+        $pricess = $this->db->table('pri_price')->select('*')->where('route_id', $trip_route_id)->where(' vehicle_type_id', $fleet_type_id)->get()->getResult();
+        if (empty($pricess)) {
+            return $this->failNotFound('Data Not Found');
+        } 
+        // $data['child_pric'] = 'Children - ' . $pricess[0]['children_price'] . $currency[0]['currency'] . ', Adult -' . $pricess[0]['price'] . $currency[0]['currency'] . ', Special-' . $pricess[0]['special_price'] . $currency[0]['currency'];
+        // return $this->respond($data);
+        $data['bankinfo'] = $this->db->table('bank_info')->select('*')->get()->getResult();
+
+        #---------BOOKED SEAT(S)-----------#
+        $bookedSeats = $this->db->table('tkt_booking AS tb')
+        ->select("
+                tb.trip_id_no,
+                SUM(tb.total_seat) AS booked_seats,
+                GROUP_CONCAT(tb.seat_numbers SEPARATOR ', ') AS booked_serial
+            ")
+            ->where('tb.trip_id_no', $trip_id_no)
+            ->like('tb.booking_date', $booking_date, 'after')
+            ->groupStart()
+            ->where("tb.tkt_refund_id IS NULL", null, false)
+            ->orWhere("tb.tkt_refund_id", 0)
+            ->orWhere("tb.tkt_refund_id", null)
+            ->groupEnd()
+            ->get()
+            ->getResult();
+        
+        $bookArray = explode(',', $bookedSeats[0]->booked_serial);
+
+        #---------FLEET SEAT(S)-----------#
+        $fleetSeats = $this->db->table("fleet_type")
+            ->select("
+                total_seat, seat_numbers,fleet_facilities
+            ")
+            ->where('id', $fleet_type_id)
+            ->get()
+            ->getResult();
+        $seatArray = explode(',', $fleetSeats[0]->seat_numbers);
+
+        $layoutset = $this->db->table("fleet_type")
+            ->select("*")
+            ->where('id', $fleet_type_id)
+            ->get()
+            ->getResult();
+            
+        $result['seatsInfo'] = $layoutset;
+        
+        if ($layoutset[0]->layout == "2-2") {
+            $separate = [2,6,10,14,18];
+        } else {
+            $separate = [];
+        }
+
+        foreach ($seatArray as $key => $value) {
+            $result['seats'][] = [
+                'row' => $key,
+                'id' => $key+1,
+                'name' => 'A'.$key+1,
+                'isAvailable' => true,
+                'isSeat' => true,
+            ];
+
+            if (in_array($key+1, $separate)) {
+                $result['seats'][] = [
+                    'row' => 00,
+                    'id' => 00,
+                    'name' => '-',
+                    'isAvailable' => false,
+                    'isSeat' => false,
+                ];
+            }
+        }
+
+        // $rowSeat = 1;
+        // $totalSeats = 1;
+        // $lastSeats = ((sizeof($seatArray) >= 3) ? (sizeof($seatArray) - 5) : sizeof($seatArray));
+
         return $this->respond($result, 200);
     }
 }
--- app/Models/TripModel.php
+++ app/Models/TripModel.php
@@ -14,36 +14,65 @@ class TripModel extends Model
         $date = $data['date'];
 
         $query = $this->db->query("SELECT
-            ta.`trip_id` AS trip_id_no,
-            ta.`route`,
-            ta.`shedule_id`,
-            tr.`name` AS trip_route_name,
-            tl1.`name` AS pickup_trip_location,
-            tl2.`name` AS drop_trip_location,
-            ta.`type`,
-            tp.`total_seat` AS fleet_seats,
-            pp.`price` AS price,
-            tr.`approximate_time` AS duration,
-            tr.`stoppage_points`,
-            tr.`distance`,
-            shedule.`start`,
-            shedule.`end`,
-            tras.`closed_by_id`
+            ta.trip_id AS trip_id_no,
+            ta.route AS trip_route_id,
+            ta.shedule_id,
+            tr.name AS trip_route_name,
+            tl1.name AS pickup_trip_location,
+            tl2.name AS drop_trip_location,
+            ta.type,
+            tp.total_seat AS fleet_seats,
+            pp.price AS price,
+            pp.children_price,
+            pp.special_price,
+            tr.approximate_time AS duration,
+            tr.stoppage_points,
+            tr.distance,
+            shedule.start,
+            shedule.end,
+            tras.closed_by_id
             FROM trip AS ta
-            LEFT JOIN shedule ON shedule.`shedule_id` = ta.`shedule_id`
-            LEFT JOIN trip_route AS tr ON tr.`id` = ta.`route`
-            LEFT JOIN trip_assign AS tras ON tras.`trip` = ta.`trip_id`
-            LEFT JOIN fleet_type AS tp ON tp.`id` = ta.`type`
-            LEFT JOIN pri_price AS pp ON pp.`route_id` = ta.`route` AND pp.`vehicle_type_id`= ta.`type`
-            LEFT JOIN trip_location AS tl1 ON tl1.`id` = tr.`start_point`
-            LEFT JOIN trip_location AS tl2 ON tl2.`id` = tr.`end_point`
-            WHERE (FIND_IN_SET('$start',tr.`stoppage_points`))
-            AND (FIND_IN_SET('$end',tr.`stoppage_points`))
-            AND ta.`type` = $f
-            AND (!FIND_IN_SET(DAYOFWEEK('$date'),ta.`weekend`))
-            GROUP BY ta.`trip_id`
+            LEFT JOIN shedule ON shedule.shedule_id = ta.shedule_id
+            LEFT JOIN trip_route AS tr ON tr.id = ta.route
+            LEFT JOIN trip_assign AS tras ON tras.trip = ta.trip_id
+            LEFT JOIN fleet_type AS tp ON tp.id = ta.type
+            LEFT JOIN pri_price AS pp ON pp.route_id = ta.route AND pp.vehicle_type_id= ta.type
+            LEFT JOIN trip_location AS tl1 ON tl1.id = tr.start_point
+            LEFT JOIN trip_location AS tl2 ON tl2.id = tr.end_point
+            WHERE (FIND_IN_SET('$start',tr.stoppage_points))
+            AND (FIND_IN_SET('$end',tr.stoppage_points))
+            AND ta.type = $f
+            AND (!FIND_IN_SET(DAYOFWEEK('$date'),ta.weekend))
+            GROUP BY ta.trip_id
         ");
 
         return $query;
+    }
+
+    public function checkSeatAvail($trip_id_no,$date)
+    {
+        $bookingResult = $this->db->table("tkt_booking AS tb")
+            ->select("SUM(tb.total_seat) AS picked")
+            ->join('trip AS ta', "ta.trip_id = tb.trip_id_no")
+            ->where('tb.trip_id_no', $trip_id_no)
+            ->like('tb.booking_date',$date,'after')
+            ->groupStart()
+                ->where("tb.tkt_refund_id IS NULL", null, false)
+                ->orWhere("tb.tkt_refund_id", 0)
+                ->orWhere("tb.tkt_refund_id", null)
+            ->groupEnd()
+            ->get();
+            
+        return $bookingResult;
+    }
+
+    public function retrieve_currency()
+    {
+        $query = $this->db->query('SELECT * FROM ws_setting');
+        
+        if ($query->getNumRows() > 0) {
+            return $query;  
+        }
+        return false;
     }
 }
