fbe5f2ff6be7256c06014ae383cef1b81bd5f35f
bayuyuhartono <bayuyuhartono@gmail.com>
2022-04-03 00:43:02 +0700

    tick

--- app/Views/documents/thermalTicket.php
+++ app/Views/documents/thermalTicket.php
@@ -69,6 +69,10 @@
         .footer p {
             margin-top: 0px;
         }
+        .page_break {
+            page-break-before: always;
+        }
+
 
     </style>
 </head>
@@ -163,6 +167,98 @@
             web:juragan99trans.id | phone:081277755485 <br> email:humas@juragan99trans.id | wa:031847653
         </p>
     </div>
+    <?php if ($tickedData->baggage == 'Bawa') { ?>
+    <div class="page_break"></div>
+    <div class="container">
+        <div id="logo" class="media" >
+            <img src="https://juragan99trans.id/images/logo-1.png"/>
+        </div>
+        <table>
+            <tr>
+                <td class="detailer">
+                    <table>
+                        <tr>
+                            <td>Nama</td>
+                            <td>:</td>
+                            <td><?= $tickedData->name ?></td>
+                        </tr>
+                        <tr>
+                            <td>No HP</td>
+                            <td>:</td>
+                            <td><?= $tickedData->phone ?></td>
+                        </tr>
+                        <tr>
+                            <td>Tanggal</td>
+                            <td>:</td>
+                            <td><?= $tickedData->booking_date ?></td>
+                        </tr>
+                        <tr>
+                            <td>Asal</td>
+                            <td>:</td>
+                            <td>Agen Pare - Kediri</td>
+                        </tr>
+                        <tr>
+                            <td>Tujuan</td>
+                            <td>:</td>
+                            <td>J99 Kalideres - Jakarta</td>
+                        </tr>
+                        <tr>
+                            <td>Tarif</td>
+                            <td>:</td>
+                            <td>Rp. <?= $tickedData->price ?></td>
+                        </tr>
+                    </table>
+                </td>
+                <td class="detailer">
+                    <table>
+                        <tr>
+                            <td>Kode Tiket</td>
+                            <td>:</td>
+                            <td><?= $tickedData->ticket_number ?></td>
+                        </tr>
+                        <tr>
+                            <td>Kelas</td>
+                            <td>:</td>
+                            <td><?= $tickedData->type ?></</td>
+                        </tr>
+                        <tr>
+                            <td>No Kursi</td>
+                            <td>:</td>
+                            <td><?= $tickedData->seat_number ?></</td>
+                        </tr>
+                        <tr>
+                            <td>Menu Makan</td>
+                            <td>:</td>
+                            <td><?= $tickedData->food_name ?></</td>
+                        </tr>
+                        <tr>
+                            <td>Bagasi</td>
+                            <td>:</td>
+                            <td><?= $tickedData->baggage ?></</td>
+                        </tr>
+                        <tr>
+                            <td>No Polisi</td>
+                            <td>:</td>
+                            <td>B 3877 NY</td>
+                        </tr>
+                    </table>
+                </td>
+                <td class="qrcode">
+                    <img src="<?= $qrcode ?>" alt="">
+                </td>
+            </tr>
+        </table>
+        <div class="center-align intro">
+            penumpang diimbau untuk datang paling lambat 30 menit sebelum jadwal keberangkatan. Hal tersebut dikarenakan pada saat proses boarding ada tahapan verifikasi berkas oleh petugas.
+        </div>
+    </div>
+    <hr>
+    <div class="footer center-align">
+        <p>
+            web:juragan99trans.id | phone:081277755485 <br> email:humas@juragan99trans.id | wa:031847653
+        </p>
+    </div>
+    <?php } ?>
 </body>
 
 </html>
