bb6bc1dd9974d63764f32b47b033de5e2428ebef
bayuyuhartono <bayuyuhartono@gmail.com>
2022-04-10 03:19:06 +0700

    mail

--- app/Views/mail/footermail.php
+++ app/Views/mail/footermail.php
@@ -1,34 +1,6 @@
 <!-- Content -->
 <table border="0" align="center" cellpadding="0" cellspacing="0" role="presentation" class="row container-padding" width="520" style="width:520px;max-width:520px;">
   <tr>
-    <td align="center">
-      <!-- Buttons -->
-      <table border="0" align="center" cellpadding="0" cellspacing="0" role="presentation" class="row" width="100%" style="width:100%;max-width:100%;">
-        <tr>
-          <td align="center">
-            <!-- column -->
-            <table border="0" align="center" cellpadding="0" cellspacing="0" role="presentation">
-              <tr>
-                <td align="center">
-                  <img style="display:block;width:100%;max-width:250px;border:0px;margin-bottom:-50px;margin-top:-35px;" data-image-edit  width="117" src="<?= base_url('assets/appstorelogo.png') ?>" border="0" editable="true" alt="icon">
-                </td>
-                <td width="10" style="width: 10px;"></td>
-                <td align="center">
-                  <img style="display:block;width:100%;max-width:250px;border:0px;margin-bottom:-50px;margin-top:-35px;" data-image-edit  width="117" src="<?= base_url('assets/playstorelogo.png') ?>" border="0" editable="true" alt="icon">
-                </td>
-              </tr>
-            </table>
-            <!-- column -->
-          </td>
-        </tr>
-      </table>
-      <!-- Buttons -->
-    </td>
-  </tr>
-  <tr>
-    <td height="10"  style="border-bottom: 4px dotted #E4E4E4;font-size:26px;line-height:26px;">&nbsp;</td>
-  </tr>
-  <tr>
     <td height="30" style="font-size:30px;line-height:30px;" Simpli>&nbsp;</td>
   </tr>
   <tr>
