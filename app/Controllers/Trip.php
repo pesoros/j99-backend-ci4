60c04fc5fa633154cc8c9e83b7878f48946361a2
bayuyuhartono <bayuyuhartono@gmail.com>
2022-06-03 03:01:54 +0700

    1-1

--- app/Controllers/Trip.php
+++ app/Controllers/Trip.php
@@ -131,7 +131,7 @@ class Trip extends ResourceController
         if ($layoutset[0]->layout == "2-2") {
             $separate = [2,6,10,14,18,22];
         } elseif ($layoutset[0]->layout == "1-1") {
-            $separate = [0,2,4,6,8,10,12,13,14,16,18,20,22,24];
+            $separate = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19,20];
         } else {
             $separate = [];
         }
