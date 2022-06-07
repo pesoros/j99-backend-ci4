a4bb9c0c967a528f10809ec901a04b7906067911
bayuyuhartono <bayuyuhartono@gmail.com>
2022-06-07 12:20:38 +0700

    pass

--- app/Controllers/LoginManifest.php
+++ app/Controllers/LoginManifest.php
@@ -29,23 +29,23 @@ class LoginManifest extends ResourceController
         helper(['form']);
         $rules = [
             'email' => 'required|valid_email',
-            'password' => 'required|min_length[6]',
+            'password' => 'required|min_length[4]',
         ];
         if (!$this->validate($rules)) {
             return $this->fail($this->validator->getErrors());
         }
 
         $model = new UserManifestModel();
-        // $user = $model->where("email", $this->request->getVar('email'))->first();
-        // if (!$user) {
-        //     return $this->failNotFound('Email Not Found');
-        // }
+        $user = $model->where("email", $this->request->getVar('email'))->first();
+        if (!$user) {
+            return $this->failNotFound('Email Not Found');
+        }
 
-        // $verify = password_verify($this->request->getVar('password'), $user['password']);
-        // $verify = md5($this->request->getVar('password')) == $user['password'];
-        // if (!$verify) {
-        //     return $this->fail('Wrong Password');
-        // }
+        $verify = password_verify($this->request->getVar('password'), $user['password']);
+        $verify = md5($this->request->getVar('password')) == $user['password'];
+        if (!$verify) {
+            return $this->fail('Wrong Password');
+        }
 
         $manifest = $this->manifestModel->getManifest($this->request->getVar('email'))->getRow();
 
