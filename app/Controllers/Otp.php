4bc9f4c6300af1683f6a5d706682edc01132d858
bayuyuhartono <bayuyuhartono@gmail.com>
2022-06-10 11:09:15 +0700

    dd

--- app/Controllers/Otp.php
+++ app/Controllers/Otp.php
@@ -145,7 +145,7 @@ class Otp extends ResourceController
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
+        CURLOPT_TIMEOUT => 10,
         CURLOPT_FOLLOWLOCATION => true,
         CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
         CURLOPT_CUSTOMREQUEST => 'POST',
